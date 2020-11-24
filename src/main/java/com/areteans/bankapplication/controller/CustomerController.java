823adfdc38269ea02add2e7a335e556e545c9351
Arun <arunsivaramakrishnan98@gmail.com>
2020-11-24 13:16:45 +0530

    23/11/2020

--- src/main/java/com/areteans/bankapplication/controller/CustomerController.java
+++ src/main/java/com/areteans/bankapplication/controller/CustomerController.java
@@ -1,6 +1,7 @@
 package com.areteans.bankapplication.controller;
 
 import com.areteans.bankapplication.models.Customer;
+import com.areteans.bankapplication.models.CustomerCopy;
 import com.areteans.bankapplication.service.CustomerService;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
@@ -15,24 +16,28 @@ import java.util.Map;
 public class CustomerController {
     private final CustomerService customerService;
 
-    @PostMapping(path ="create", consumes = MediaType.APPLICATION_JSON_VALUE)
-    public Customer create(@RequestBody Customer customer) {
-        return customerService.create(customer);
-    }
-
-    @PostMapping(path= "update", consumes = MediaType.APPLICATION_JSON_VALUE)
-    public Long update(@RequestBody Map<String,String> update) {
-        return customerService.updateBalance(update.get("accountno"), Long.valueOf(update.get("balance")));
-    }
-
+//    @PostMapping(path ="create", consumes = MediaType.APPLICATION_JSON_VALUE)
+//    public Customer create(@RequestBody Customer customer) {
+//        return customerService.create(customer);
+//    }
+//
+//    @PostMapping(path= "update", consumes = MediaType.APPLICATION_JSON_VALUE)
+//    public Long update(@RequestBody Map<String,String> update) {
+//        return customerService.updateBalance(update.get("accountno"), Long.valueOf(update.get("balance")));
+//    }
+//
     @GetMapping(path= "read")
-    public Customer read(@RequestParam("cid") Long cid ) {
+    public CustomerCopy read(@RequestParam("cid") Long cid ) {
         return customerService.readdetails(cid);
     }
+//
+//    @PostMapping(path= "delete", consumes = MediaType.APPLICATION_JSON_VALUE)
+//    public void delete(@RequestBody Map<String,String> delete) {
+//        customerService.delete(Long.valueOf(delete.get("cid")));
+//    }
 
-    @PostMapping(path= "delete", consumes = MediaType.APPLICATION_JSON_VALUE)
-    public void delete(@RequestBody Map<String,String> delete) {
-        customerService.delete(Long.valueOf(delete.get("cid")));
+    @PostMapping(path ="create", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public CustomerCopy create(@RequestBody CustomerCopy customer) {
+        return customerService.create(customer);
     }
-
 }
--- src/main/java/com/areteans/bankapplication/service/CustomerService.java
+++ src/main/java/com/areteans/bankapplication/service/CustomerService.java
@@ -1,8 +1,9 @@
 package com.areteans.bankapplication.service;
 
-import com.areteans.bankapplication.models.Account;
-import com.areteans.bankapplication.models.Customer;
-import com.areteans.bankapplication.models.Details;
+import com.areteans.bankapplication.models.*;
+import com.areteans.bankapplication.repository.AccountRepository;
+import com.areteans.bankapplication.repository.CustomerRepository;
+import com.areteans.bankapplication.repository.DetailsRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Service;
@@ -11,29 +12,40 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Map;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class CustomerService {
     private final JdbcTemplate jdbcTemplate;
+    private final DetailsRepository detailsRepository;
+    private final AccountRepository accountRepository;
+    private final CustomerRepository customerRepository;
 
-    public Customer create(Customer customer) {
-        customer.getDetail();
-        customer.getAccount();
-        Map<String, Object> map1 = jdbcTemplate.queryForMap("insert into details(name,address,contact) values(?,?,?) RETURNING detailid",
-                customer.getDetail().getName(),
-                customer.getDetail().getAddress(),
-                customer.getDetail().getContact());
-
-        Map<String, Object> map2 = jdbcTemplate.queryForMap("insert into account(balance,accountno,acctype) values(?,?,?) RETURNING accid",
-                0L,
-                customer.getAccount().getAccountno(),
-                customer.getAccount().getAcctype());
-        Map<String,Object> map3= jdbcTemplate.queryForMap("insert into customer(detailid,accid) values(?,?) RETURNING cid",
-                map1.get("detailid"),
-                map2.get("accid"));
-                customer.setCid((Long)map3.get("cid"));
+    public CustomerCopy create(CustomerCopy customer) {
+        AccountJPA accountJPA = accountRepository.save(customer.getAccountJPA());
+        DetailsJPA detailsJPA = detailsRepository.save(customer.getDetailsJPA());
+        CustomerJPA customerJPA = new CustomerJPA();
+        customerJPA.setDetailid(detailsJPA.getDetailid());
+        customerJPA.setAccid(accountJPA.getAccid());
+        customerJPA = customerRepository.save(customerJPA);
+        customer.setCid(customerJPA.getCid());
         return customer;
+
+//        Map<String, Object> map1 = jdbcTemplate.queryForMap("insert into details(name,address,contact) values(?,?,?) RETURNING detailid",
+//                customer.getDetail().getName(),
+//                customer.getDetail().getAddress(),
+//                customer.getDetail().getContact());
+//
+//        Map<String, Object> map2 = jdbcTemplate.queryForMap("insert into account(balance,accountno,acctype) values(?,?,?) RETURNING accid",
+//                0L,
+//                customer.getAccount().getAccountno(),
+//                customer.getAccount().getAcctype());
+//        Map<String,Object> map3= jdbcTemplate.queryForMap("insert into customer(detailid,accid) values(?,?) RETURNING cid",
+//                map1.get("detailid"),
+//                map2.get("accid"));
+//                customer.setCid((Long)map3.get("cid"));
+//        return customer;
     }
 
     public Long updateBalance(String accountno , Long balance) {
@@ -43,25 +55,38 @@ public class CustomerService {
         return (Long) map.get("balance");
     }
 
-    public Customer readdetails(Long cid) {
-        Map<String,Object> map= jdbcTemplate.queryForMap("select * from customer where cid=?", cid);
-        Long detailid= (Long) map.get("detailid");
-        Long accid= (Long) map.get("accid");
-        Details obj= jdbcTemplate.queryForObject("select * from details where detailid=" + detailid, new RowMapper<Details>() {
-            @Override
-            public Details mapRow(ResultSet resultSet, int i) throws SQLException {
-                return new Details(resultSet.getString("name"), resultSet.getString("address"), resultSet.getLong("contact"));
-
-            }
+    public CustomerCopy readdetails(Long cid) {
+        Optional<CustomerJPA>customer =customerRepository.findById(cid);
+        if(customer.isPresent()) {
+            Long detailid = customer.get().getDetailid();
+            Long accid = customer.get().getAccid();
+            Optional<DetailsJPA> detailsJPA = detailsRepository.findById(detailid);
+            Optional<AccountJPA> accountJPA = accountRepository.findById(accid);
+            CustomerCopy customerCopy = new CustomerCopy(detailsJPA.get(),accountJPA.get(),cid);
+            return customerCopy;
         }
-        );
-        Account obj1= jdbcTemplate.queryForObject("select * from account where accid=" + accid, new RowMapper<Account>() {
-            @Override
-            public Account mapRow(ResultSet resultSet, int i) throws SQLException {
-                return new Account(resultSet.getLong("balance"), resultSet.getString("accountno"), resultSet.getString("acctype"));
-            }
-        });
-        return new Customer(obj,obj1,cid);
+        else {
+            return null;
+        }
+
+//        Map<String,Object> map= jdbcTemplate.queryForMap("select * from customer where cid=?", cid);
+//        Long detailid= (Long) map.get("detailid");
+//        Long accid= (Long) map.get("accid");
+//        Details obj= jdbcTemplate.queryForObject("select * from details where detailid=" + detailid, new RowMapper<Details>() {
+//            @Override
+//            public Details mapRow(ResultSet resultSet, int i) throws SQLException {
+//                return new Details(resultSet.getString("name"), resultSet.getString("address"), resultSet.getLong("contact"));
+//
+//            }
+//        }
+//        );
+//        Account obj1= jdbcTemplate.queryForObject("select * from account where accid=" + accid, new RowMapper<Account>() {
+//            @Override
+//            public Account mapRow(ResultSet resultSet, int i) throws SQLException {
+//                return new Account(resultSet.getLong("balance"), resultSet.getString("accountno"), resultSet.getString("acctype"));
+//            }
+//        });
+//        return new Customer(obj,obj1,cid);
     }
 
     public void delete(Long cid) {
@@ -70,4 +95,6 @@ public class CustomerService {
         jdbcTemplate.update("delete from account where accid=?", map.get("accid"));
         jdbcTemplate.update("delete from details where detailid=?", map.get("detailid"));
     }
+
+
 }
